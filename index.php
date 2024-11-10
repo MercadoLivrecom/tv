392db0783176bedda53c63155b415de585ec2d78
MercadoLivrecom <alekziinamassa@gmail.com>
2024-11-10 13:06:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3xM-N-y-2jZmdOhVYqcYqGuFNIfpEjaKgcA&usqp=CAU">
+    <title>TV ONLINE GRATIS E VIP</title>
+    <style>
+        body {
+            background-color: #041364;
+            color: #d3b366;
+            font-size: 17px;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            display: none; /* Removendo o cabeçalho */
+        }
+
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin-top: 20px; /* Reduzindo a margem superior */
+        }
+
+        .category {
+            width: calc(50% - 20px); /* Exibir dois quadrados por linha */
+            height: 150px; /* Tamanho dos quadrados */
+            margin: 10px;
+            cursor: pointer;
+            position: relative;
+            background-color: rgba(0, 0, 0, 0.7);
+            border-radius: 5px;
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            transition: background-color 0.3s;
+        }
+
+        .category .image {
+            width: 100%;
+            height: 100px; /* Tamanho da imagem */
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        .category .text {
+            font-size: 16px;
+            margin-top: 10px;
+            color: white;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        /* Estilos para os botões */
+        .button-wrapper {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+            padding: 10px 0; /* Espaçamento interno do container dos botões */
+        }
+
+        .button {
+            width: 150px;
+            height: 40px;
+            margin: 5px;
+            background-color: #d3b366; /* Cor de fundo dourada */
+            border: 2px solid #d3b366;
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #041364;
+            font-size: 14px;
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: uppercase;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .button:hover {
+            background-color: transparent; /* Fundo transparente ao passar o mouse */
+            color: #d3b366;
+        }
+
+        .button:focus {
+            outline: none;
+        }
+
+    </style>
+</head>
+<body>
+    <!-- Container para os botões -->
+    <div class="button-wrapper">
+        <button class="button" onclick="window.location.href='tv/vip.php'">Grupo 03</button>
+        <button class="button" onclick="window.location.href='vip1.php'">VIP R$ 29,99</button>
+        <button class="button" onclick="window.location.href='login.php'">Liberar VIP</button>
+    
+  </div>
+    <div class="container">
+        <div class="category" onclick="window.location.href='tv/1.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/1tvNNs2z/Picsart-23-07-09-17-27-25-780.png');"></div>
+            <h2 class="text">1 SBT BRASILIA</h2>
+        </div>
+
+        <div class="category" onclick="window.location.href='tv/2.php'">
+            <div class="image" style="background-image: url('https://listaiptv.gratis/logos/imagens/band.png');"></div>
+            <h2 class="text">2 BAND TV</h2>
+        </div>
+
+
+        <div class="category" onclick="window.location.href='tv/3.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/qMJ452NW/Picsart-23-07-09-17-33-25-901.png');"></div>
+            <h2 class="text">3 RECORDTV</h2>
+        </div>
+
+
+
+        <div class="category" onclick="window.location.href='tv/4.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/rsPT4PqW/Picsart-23-07-09-17-45-09-649.png');"></div>
+            <h2 class="text">4 GLOBO </h2>
+        </div>
+
+
+        <div class="category" onclick="window.location.href='tv/5.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/bNgCmtnC/Picsart-23-07-09-17-53-15-228.png');"></div>
+            <h2 class="text">5 REDE TV</h2>
+        </div>
+
+
+
+        <div class="category" onclick="window.location.href='tv/6.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/ryngWk23/Picsart-23-07-09-18-08-31-512.png');"></div>
+            <h2 class="text">6 Rede Clone - WebTV</h2>
+        </div>
+
+
+        <div class="category" onclick="window.location.href='tv/7.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/vmbKYwnB/Picsart-23-07-09-18-22-17-510.png');"></div>
+            <h2 class="text">7 cartoon HD</h2>
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/8.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/j5QPRBzs/Picsart-23-07-09-18-31-31-625.png');"></div>
+            <h2 class="text">8 ALL Sports</h2>
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/9.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/RZNB0f7M/Picsart-23-07-31-17-19-22-537.png');"></div>
+            <h2 class="text">9 ANIMES 24H</h2>
+
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/10.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/Jh3QvF9S/Picsart-23-07-12-16-19-16-360.png');"></div>
+            <h2 class="text">10 TV CINE</h2>
+
+
+
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/11.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/HxntFQFb/Picsart-23-07-12-19-03-29-824.png');"></div>
+            <h2 class="text">11 tv Cine PIX</h2>
+
+
+
+
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/12.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/FKbFB6Xd/Picsart-23-07-13-11-07-59-317.png');"></div>
+            <h2 class="text">12 Tv Xtreme</h2>
+
+
+
+
+
+
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/13.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/RVWVMwSw/Picsart-23-07-18-23-34-40-169.png');"></div>
+            <h2 class="text">13 telecine</h2>
+
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/14.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/qMhZBv9N/Picsart-23-07-18-23-24-03-555.png');"></div>
+            <h2 class="text">14 REDE BRASIL</h2>
+
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/15.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/2yC3D55t/Picsart-23-07-19-12-25-24-290.png');"></div>
+            <h2 class="text">15 TV MEGA CINE</h2>
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/16.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/gjh6K00t/Picsart-23-07-19-13-16-23-485.png');"></div>
+            <h2 class="text">16 CHAPOLIN 24 h</h2>
+
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/17.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/1t9344Bm/Picsart-23-07-31-16-36-59-095.png');"></div>
+            <h2 class="text">17 CHANNEL</h2>
+
+
+  </div>
+        <div class="category" onclick="window.location.href='tv/18.php'">
+            <div class="image" style="background-image: url('https://i.postimg.cc/wvZ2bq0h/Picsart-23-07-30-14-30-42-973.png');"></div>
+            <h2 class="text">18 ISTV</h2>
+
+
+
+        
+
+
+  <?php
+    // Gera um código aleatório
+    $randomCode = uniqid();
+  
+    // Use o código gerado para exibir na barra do navegador
+    echo '<script>window.history.pushState("", "", "?codigo=' . $randomCode . '");</script>';
+    ?>
+
+</body>
+</html>
